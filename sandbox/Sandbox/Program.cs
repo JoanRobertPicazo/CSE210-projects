1bb4ba0e00890d3063f71d8c1cf27931115878ab
Joan Picazo <joanpicazo@Joans-MacBook-Pro.local>
2024-06-10 15:07:50 -0600

    added for loop

--- sandbox/Sandbox/Program.cs
+++ sandbox/Sandbox/Program.cs
@@ -5,5 +5,9 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Sandbox World!");
+        for(int j=0; j < 10; j++)
+        {
+            Console.WriteLine   (j);
+        }
     }
 }
