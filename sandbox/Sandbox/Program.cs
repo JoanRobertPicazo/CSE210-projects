5f145233f66c388cd143c51a42d43e2e7dafecfd
Joan Picazo <joanrpicazo@gmail.com>
2024-06-11 08:14:43 -0600

    Made adjustments to the code

--- sandbox/Sandbox/Program.cs
+++ sandbox/Sandbox/Program.cs
@@ -4,7 +4,7 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Sandbox World!!!");
+        Console.WriteLine("Hello Sandbox World!");
         for(int j=0; j < 10; j++)
         {
             Console.WriteLine   (j);
