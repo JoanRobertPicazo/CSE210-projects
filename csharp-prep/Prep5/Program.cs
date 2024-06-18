81ce9841e5decd5bfd6020dc07bc72c906df34dd
Joan Picazo <joanrpicazo@gmail.com>
2024-06-17 19:44:14 -0600

    Finished prep 5

--- csharp-prep/Prep5/Program.cs
+++ csharp-prep/Prep5/Program.cs
@@ -4,6 +4,19 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep5 World!");
+        Console.WriteLine("Welcome to the program!");
+
+        Console.Write("Please enter your name: ");
+        string name = Console.ReadLine();
+
+        Console.Write("Please enter your favorite number: ");
+        string numberAnswer = Console.ReadLine();
+        int favoriteNumber = int.Parse(numberAnswer);
+
+        int favoriteNumberSquared = favoriteNumber * favoriteNumber;
+
+        Console.WriteLine($"{name}, the square of your number is {favoriteNumberSquared}");
+
+
     }
 }
