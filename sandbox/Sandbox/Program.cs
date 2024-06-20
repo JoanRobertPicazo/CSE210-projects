2d584480061993315f2d1e101803fec7781b02b7
Joan Picazo <joanrpicazo@gmail.com>
2024-06-20 14:55:42 -0600

    Completed Learning  02

--- sandbox/Sandbox/Program.cs
+++ sandbox/Sandbox/Program.cs
@@ -2,11 +2,19 @@ using System;
 
 
 
+
+
 class Program
 {
     static void Main(string[] args)
     {
         Console.WriteLine("Bonjour Tout Le Monde!");
+
+        List<int> numbers = new List<int>{1,2,32};
+
+        numbers.Add(109);
+        numbers.Add(0);
+
 
         Circle myCircle = new Circle(10);
         Console.WriteLine($"{myCircle.GetArea()}");
