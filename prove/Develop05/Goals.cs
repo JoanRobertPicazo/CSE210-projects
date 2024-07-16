7370549a02a32c30bbe7ba0aa722f810cd96b221
Joan Picazo <joanrpicazo@gmail.com>
2024-07-15 19:00:35 -0600

    Completed Learning 03

---
+++ prove/Develop05/Goals.cs
@@ -0,0 +1,42 @@
+using System;
+
+// _description: string​
+
+// _numberOfPoints: string​
+
+// _status: bool
+
+// Goal(name: string, description: string, points: int, status: bool)​
+
+// Goal()​
+
+
+
+// GetDescription(): string​
+
+// SetDescription(): void​
+
+// GetPoints(): int​
+
+// SetPoints(): int​
+
+// GetStatus(): bool​
+
+// MarkComplete(): int​
+
+// {virtual} ListGoal(): string​
+
+// {override} ToString(): string​
+
+// {abstract} GetGoalType(): string​
+
+// {abstract} RecordEvent(): int​
+
+// {abstract} RunGoal: void
+
+public abstract class Goal
+
+public string GetName()
+{
+    reutrn _name;
+}
---
+++ prove/Develop05/Menu.cs
@@ -0,0 +1,5 @@
+using System;
+
+DisplayMenu(): int​
+
+DisplayCreateGoalMenu(): int
